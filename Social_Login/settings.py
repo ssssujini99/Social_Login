51a8958827e53525699c32be0e6f37a0400d5ec5
ssssujini99 <sssssjin99@naver.com>
2022-02-08 20:21:31 +0900

    feat(settings.py): Mysql 설정 추가

--- Social_Login/settings.py
+++ Social_Login/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'Social_Login.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
