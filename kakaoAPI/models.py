531550205368d2aafddedb32e2423e0731c372f6
ssssujini99 <sssssjin99@naver.com>
2022-02-09 14:00:09 +0900

    feat(kakaoAPI/models.py): User모델 커스텀

--- kakaoAPI/models.py
+++ kakaoAPI/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, nickname, password):
+
+        if not email:
+            raise ValueError('must have user email')
+        if not nickname:
+            raise ValueError('must have user nickname')
+        if not password:
+            raise ValueError('must have user password')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password):
+
+        user = self.create_user(
+            email=self.normalize_email(email),
+            nickname=nickname,
+            password=password
+        )
+        user.is_admin = True  # 슈퍼유저 권한 부여
+        user.is_superuser = True  # 슈퍼유저 권한 부여
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+    nickname = models.CharField(
+        max_length=10,
+        unique=True
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nickname', ]
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
